e0ea61899d238035af3b6ec415d8ce8e431792d4
arun <arunkumarreddy.n@cognizant.com>
2014-08-19 02:20:43 +0530

    test project

---
+++ .tmp/public/jst.js
@@ -0,0 +1,11 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["assets/templates/index.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<!DOCTYPE html>\r\n<html ng-app="barmaddenApp">\r\n\r\n<body>\r\n\r\n<div>\r\n \t<p>Name: <input type="text" ng-model="name"></p>\r\n \t<p ng-bind="name"></p>\r\n</div>\r\n\r\n<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>\r\n\r\n</body>\r\n</html>';
+
+}
+return __p
+};
